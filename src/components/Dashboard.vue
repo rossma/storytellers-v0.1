004a879bb6a9b40da4a461e6a42dda5b8cf9add4
Ross Mahony <ross.mahony@amt-sybex.com>
2017-06-17 12:54:21 +0100

    Added vuex, vue-router and email authentication to firebase

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>Dashboard</h3>
+    <button @click="signOut">Sign Out</button>
+    {{ $store.state }}
+  </div>
+</template>
+
+<script>
+  import { firebaseApp } from '../firebaseApp'
+
+  export default {
+    data() {
+      return {
+        tst: ''
+      }
+    },
+    methods: {
+      signOut() {
+        this.$store.dispatch('signOut')
+        firebaseApp.auth().signOut()
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import { firebaseApp } from './firebaseApp'
 
 var config = require('config');
 
+Vue.use(VueRouter)
+
+import store from './store'
+
+import Dashboard from './components/Dashboard.vue'
+import SignIn from './components/auth/Signin.vue'
+import SignUp from './components/auth/Signup.vue'
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/dashboard', component: Dashboard },
+    { path: '/signIn', component: SignIn },
+    { path: '/signUp', component: SignUp }
+  ]
+})
+
 firebaseApp.auth().onAuthStateChanged(user => {
   if (user) {
-    console.log("user exists")
+    store.dispatch('signIn', user)
+    router.push('/dashboard')
   } else {
-    console.log("user doesnt exist:" + config.FIREBASE.API_KEY)
+    router.replace('/signin')
   }
 })
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
